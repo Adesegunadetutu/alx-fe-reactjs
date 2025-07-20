dddb64b25ac3571aee46c4960ba54800c34fa835
Adesegun Adetutu <adesegunadetutu20@gmail.com>
2025-07-20 16:19:52 +0100

    counter exercise

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 
 
@@ -16,6 +17,9 @@ function App() {
     <>
      <Header/>
              <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+             <div>
+      <Counter />
+    </div>
             
 <Footer/>
     </>
