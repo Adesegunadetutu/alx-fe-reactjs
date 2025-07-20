a4a87cec8d0824c99f784c2364123cba8c46f74f
Adesegun Adetutu <adesegunadetutu20@gmail.com>
2025-07-20 17:03:21 +0100

    userprofile

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,13 +1,11 @@
-
-import React, { useContext } from 'react';
-import UserContext from './UserContext';
+import { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 function UserDetails() {
   const userData = useContext(UserContext);
 
   return (
     <div>
-      <h2>User Profile</h2>
       <p>Name: {userData.name}</p>
       <p>Email: {userData.email}</p>
     </div>
